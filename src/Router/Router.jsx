c899cecf703a9dbdee557119a647a590c98a91dc
Moshe Shabes <moshnaf@gmail.com>
2022-10-03 10:22:13 -0500

    d

--- src/Router/Router.jsx
+++ src/Router/Router.jsx
@@ -9,13 +9,6 @@ export default function Router() {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
-      {/* <Route path="/login" element={<Login />} />
-      <Route path="/registration" element={<Registration />} />
-      <Route path="/forgot-password" element={<ForgotPassword />} />
-      <Route path="/reset-password/:token" element={<ResetPassword />} />
-      <Route path="/metricas" element={<PrivateRoute component={<Metricas />} />} />
-      <Route path="/my-account" element={<PrivateRoute component={<MyAccount />} />} />
-      <Route path="/superadmin" element={<SuperPrivateRoute component={<SuperadminPage />} />} /> */}
 
       <Route path="/*" element={<div>ERROR 404</div>} />
     </Routes>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -8,33 +8,23 @@ export default function Home() {
   const { user, onConnect } = useContext(UserContext)
 
   async function createWidget() {
-    // Function to call your server-side to generate the access_token and retrieve the your access token
-    get("/get_token").then(({ access }) => {
-      // const { access } = data;
-      console.log(access)
-      window.belvoSDK.createWidget(access, config).build()
-    })
-    const callback = () => { }
-    const successCallbackFunction = (link, institution) => {
-      onConnect(link)
-      // Do something with the link and institution,
-      // such as associate it with your registered user in your database.
-    }
-    const onExitCallbackFunction = (data) => {
-      // Do something with the exit data.
-    }
-    const onEventCallbackFunction = (data) => {
-      // Do something with the exit data.
-    }
     const config = {
-
-      // Add your startup configuration here.
       locale: "es",
       country_codes: ["MX"],
-
       callback: (link, institution) => successCallbackFunction(link, institution),
       onExit: (data) => onExitCallbackFunction(),
       onEvent: (data) => onEventCallbackFunction()
+    }
+
+    get("/get_token").then(({ access }) => {
+      window.belvoSDK.createWidget(access, config).build()
+    })
+    const successCallbackFunction = (link, institution) => {
+      onConnect(link)
+    }
+    const onExitCallbackFunction = (data) => {
+    }
+    const onEventCallbackFunction = (data) => {
     }
   }
 
